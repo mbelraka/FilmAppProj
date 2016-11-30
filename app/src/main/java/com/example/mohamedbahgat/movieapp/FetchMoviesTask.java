1789fbe125fa32bd6cc1e8ab34c127c08622dd47
mbelraka <mohamed.elrakayby@gmail.com>
2016-12-01 00:10:18 +0200

    adding data processing in fragment

--- app/src/main/java/com/example/mohamedbahgat/movieapp/FetchMoviesTask.java
+++ app/src/main/java/com/example/mohamedbahgat/movieapp/FetchMoviesTask.java
@@ -3,6 +3,7 @@ package com.example.mohamedbahgat.movieapp;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.SystemClock;
+import android.support.annotation.NonNull;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,95 +14,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by MohamedBahgat on 2016-11-25.
  */
 
-public class FetchMoviesTask extends AsyncTask<String, Void , String[]>{
-
-    @Override
-    protected String[] doInBackground(String... strings) {
-
-        try{
-
-            HttpURLConnection connection = null;
-
-            BufferedReader reader = null;
-
-            String url_path = generateURL(strings[0]);
-
-            URL url = new URL(url_path);
-
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-
-            InputStream inputStream = connection.getInputStream();
-
-            if(inputStream == null){
-
-                return null;
-            }
-
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            StringBuffer stringBuffer = new StringBuffer();
-            String line;
-
-            while ((line = reader.readLine()) != null){
-
-                stringBuffer.append(line + "\n");
-            }
-
-            if(stringBuffer.length() == 0){
-                return  null;
-            }
-
-            String[] data = getDataFromJSON(stringBuffer.toString());
-
-        } catch(Exception e){
-
-            System.out.println("exception" + e.getMessage());
-
-        }
-
-        return new String[0];
-    }
-
-    protected String[] getDataFromJSON(String jsonString){
-
-        try{
-
-            JSONObject jsonObject = new JSONObject(jsonString);
-            int pageNum = jsonObject.getInt("page");
-            JSONArray itemsArray = jsonObject.getJSONArray("results");
-
-
-        } catch (Exception e){
-
-            System.out.println("exception" + e.getMessage());
-        }
-
-
-        return null;
-    }
-
-    protected String generateURL(String param){
-
-        try{
-
-            Uri builder = Uri.parse(BuildConfig.URL).buildUpon().
-                    appendQueryParameter(BuildConfig.SortByParameter,
-                            (param.equals("popularity")? BuildConfig.PopularityParameter : BuildConfig.RateParameter)).
-                    appendQueryParameter(BuildConfig.APIParameter, BuildConfig.APIKEY).build();
-
-            return builder.toString();
-
-        } catch (Exception e){
-
-        }
-
-        return null;
-    }
-}
--- app/src/main/java/com/example/mohamedbahgat/movieapp/PortraitFragment.java
+++ app/src/main/java/com/example/mohamedbahgat/movieapp/PortraitFragment.java
@@ -1,8 +1,27 @@
 package com.example.mohamedbahgat.movieapp;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
 
 /**
  * Created by MohamedBahgat on 2016-11-25.
@@ -10,7 +29,8 @@ import android.widget.ArrayAdapter;
 
 public class PortraitFragment extends Fragment {
 
-    ArrayAdapter<String> movieAdapter;
+    private MovieAdapter movieAdapter;
+    private GridView gridView;
 
     public PortraitFragment(){
 
@@ -21,8 +41,134 @@ public class PortraitFragment extends Fragment {
 
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.main_fragment, container, false);
+        gridView = (GridView) rootView.findViewById(R.id.gridView);
+
+        movieAdapter = new MovieAdapter(this.getContext());
+
         FetchMoviesTask fetchTask = new FetchMoviesTask();
-        fetchTask.execute();
+        fetchTask.execute("popularity", "1");
+
+        gridView.setAdapter(movieAdapter);
+
+        return rootView;
+    }
+
+    public class FetchMoviesTask extends AsyncTask<String, Void , List<Movie>> {
+
+        @Override
+        protected List<Movie> doInBackground(String... strings) {
+
+            try{
+
+                HttpURLConnection connection = null;
+
+                BufferedReader reader = null;
+
+                String url_path = generateURL(strings[0], Integer.parseInt(strings[1]));
+
+                URL url = new URL(url_path);
+
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                if(inputStream == null){
+
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                StringBuffer stringBuffer = new StringBuffer();
+                String line;
+
+                while ((line = reader.readLine()) != null){
+
+                    stringBuffer.append(line + "\n");
+                }
+
+                if(stringBuffer.length() == 0){
+                    return  null;
+                }
+
+                List<Movie> data = getDataFromJSON(stringBuffer.toString());
+
+                return data;
+
+            } catch(Exception e){
+
+                System.out.println("exception" + e.getMessage());
+
+            }
+
+            return null;
+        }
+
+        protected List<Movie> getDataFromJSON(String jsonString){
+
+            try{
+
+                JSONObject jsonObject = new JSONObject(jsonString);
+                int pageNum = jsonObject.getInt("page");
+                JSONArray itemsArray = jsonObject.getJSONArray("results");
+                List<Movie> movies = new ArrayList<Movie>();
+
+                for(int i = 0; i < itemsArray.length(); i++){
+
+                    JSONObject movieJson = itemsArray.getJSONObject(i);
+                    String title = movieJson.getString("original_title");
+                    String poster_path = movieJson.getString("poster_path");
+                    double popularity = movieJson.getDouble("popularity");
+                    double rating = movieJson.getDouble("vote_average");
+
+                    Movie movie = new Movie(title, poster_path, popularity, rating);
+
+                    movies.add(movie);
+                }
+
+                return movies;
+
+            } catch (Exception e){
+
+                System.out.println("exception" + e.getMessage());
+            }
+
+
+            return null;
+        }
+
+        protected String generateURL(String param, int pageNum){
+
+            try{
+
+                Uri builder = Uri.parse(BuildConfig.URL).buildUpon().
+                        appendQueryParameter(BuildConfig.SortByParameter,
+                                (param.equals("popularity")? BuildConfig.PopularityParameter : BuildConfig.RateParameter)).
+                        appendQueryParameter(BuildConfig.APIParameter, BuildConfig.APIKEY).build();
+
+                return builder.toString();
+
+            } catch (Exception e){
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(List<Movie> movies) {
+
+            movieAdapter.setMovies(movies);
+            movieAdapter.notifyDataSetChanged();
+        }
     }
 
 }
