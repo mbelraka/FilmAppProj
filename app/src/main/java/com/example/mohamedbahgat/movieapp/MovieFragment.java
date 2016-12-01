d576ac02738bea14f1643178422a64c8a0d7fc77
mbelraka <mohamed.elrakayby@gmail.com>
2016-12-01 21:35:54 +0200

    updating movie info view

--- app/src/main/java/com/example/mohamedbahgat/movieapp/MovieFragment.java
+++ app/src/main/java/com/example/mohamedbahgat/movieapp/MovieFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -41,11 +40,18 @@ public class MovieFragment extends Fragment {
             Movie movie = bundle.getParcelable("movie");
 
             if(movie != null){
+
+                TextView movie_title = (TextView)rootView.findViewById(R.id.movie_info_title);
+                movie_title.setText(movie.getTitle());
+
                 ImageView movie_poster = (ImageView)rootView.findViewById(R.id.poster);
                 Picasso.with(this.getContext()).load(BuildConfig.imageURL + movie.getPoster_path()).into(movie_poster);
 
                 TextView movie_release_date = (TextView)rootView.findViewById(R.id.release_year);
                 movie_release_date.setText(movie.getReleaseDate());
+
+                TextView movie_rating = (TextView)rootView.findViewById(R.id.rating);
+                movie_rating.setText(movie.getRatingString());
 
                 TextView movie_overview = (TextView)rootView.findViewById(R.id.overview);
                 movie_overview.setText(movie.getOverview());
