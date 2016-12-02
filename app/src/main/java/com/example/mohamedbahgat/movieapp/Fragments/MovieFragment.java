6e0a467577ef1ba810a9981846b85d1a0c1d802b
mbelraka <mohamed.elrakayby@gmail.com>
2016-12-03 00:47:07 +0200

    adding reviews

--- app/src/main/java/com/example/mohamedbahgat/movieapp/Fragments/MovieFragment.java
+++ app/src/main/java/com/example/mohamedbahgat/movieapp/Fragments/MovieFragment.java
@@ -12,11 +12,14 @@ import android.view.ViewGroup;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.mohamedbahgat.movieapp.BuildConfig;
 import com.example.mohamedbahgat.movieapp.R;
 import com.example.mohamedbahgat.movieapp.models.Movie;
+import com.example.mohamedbahgat.movieapp.models.Review;
 import com.example.mohamedbahgat.movieapp.models.Trailer;
 import com.squareup.picasso.Picasso;
 
@@ -73,6 +76,14 @@ public class MovieFragment extends Fragment {
                     TrailerAdapter trailerAdapter = new TrailerAdapter(this.getContext());
                     trailers_list.setAdapter(trailerAdapter);
                     trailerAdapter.setTrailers(movie.getTrailers());
+                }
+
+                if(movie.getReviews() != null){
+
+                    ListView reviews_list = (ListView) rootView.findViewById(R.id.reviews_list);
+                    ReviewAdapter reviewAdapter = new ReviewAdapter(this.getContext());
+                    reviews_list.setAdapter(reviewAdapter);
+                    reviewAdapter.setReviews(movie.getReviews());
                 }
 
             }
@@ -234,6 +245,129 @@ public class MovieFragment extends Fragment {
 
         public void setTrailers(List<Trailer> trailers) {
             this.trailers = trailers;
+        }
+    }
+
+    public class ReviewAdapter implements ListAdapter {
+
+        private Context context;
+        private List<Review> reviews;
+
+        public ReviewAdapter(Context context){
+
+            this.context = context;
+        }
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return false;
+        }
+
+        @Override
+        public boolean isEnabled(int i) {
+            return false;
+        }
+
+        @Override
+        public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
+        }
+
+        @Override
+        public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+        }
+
+        @Override
+        public int getCount() {
+
+            if(reviews == null){
+                return 0;
+            }
+
+            return reviews.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            if(view == null){
+
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                view = inflater.inflate(R.layout.review_item_layout, null);
+            }
+
+            TextView review_url_body = (TextView) view.findViewById(R.id.review_url_body);
+            final String review_url_string = getReviews().get(i).getLink();
+            review_url_body.setText(review_url_string);
+
+            TextView review_author_body = (TextView) view.findViewById(R.id.review_author_body);
+            review_author_body.setText(getReviews().get(i).getAuthor());
+
+            TextView review_content_body = (TextView) view.findViewById(R.id.review_content_body);
+            review_content_body.setText(getReviews().get(i).getContent());
+
+            review_url_body.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+
+                    try{
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(review_url_string)));
+                    }
+                    catch (Exception e){
+
+                    }
+                }
+            });
+
+            return view;
+
+        }
+
+        @Override
+        public int getItemViewType(int i) {
+            return 0;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        public Context getContext() {
+            return context;
+        }
+
+        public void setContext(Context context) {
+            this.context = context;
+        }
+
+        public List<Review> getReviews() {
+            return reviews;
+        }
+
+        public void setReviews(List<Review> reviews) {
+            this.reviews = reviews;
         }
     }
 }
